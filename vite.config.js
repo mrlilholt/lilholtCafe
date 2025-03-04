da7e09f63445f1c7a64c6665f3aadcea19f57a6b
alilholt <alilholt@MBRental-Zara.Burke>
2025-03-03 21:46:21 -0500

    Fix image path and Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: './',
-    plugins: [react()],
-    root: 'public',
-    build: {
-        outDir: '../dist'
-    }
+  base: '/',
+  plugins: [react()],
+  root: 'public',
+  publicDir: 'public', // Ensure Vite copies the public directory
+  build: {
+    outDir: '../dist'
+  }
 });
