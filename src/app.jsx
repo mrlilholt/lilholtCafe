f87b547014dd193287fb56dda0c3881ed6e2999c
alilholt <alilholt@MBRental-Zara.Burke>
2025-03-04 19:15:22 -0500

    color&font changeup

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Clock from './components/Clock.jsx';
 import DinnerDisplay from './components/DinnerDisplay.jsx';
-import YouTubePlayer from './components/YouTubePlayer.jsx';
 import Weather from './components/Weather.jsx';
 import AudioPlayer from './components/AudioPlayer.jsx';
 import './styles/main.css';
@@ -18,9 +17,11 @@ const App = () => {
                 <div className="card">
                     <DinnerDisplay />
                 </div>
+                {/*
                 <div className="card youtube-card">
                     <YouTubePlayer playlistId="PLjwiCt7m0sVp186Ox3eKijOO2xBHMYAbE" />
                 </div>
+                */}
                 <div className="card">
                     <Weather />
                 </div>
